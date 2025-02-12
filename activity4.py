4b948dd2517fe76113bfa99f1661c893b575f294
POTATOPROGAMMER <codercode1010@gmail.com>
2025-02-12 19:25:38 +0400

    commit4

---
+++ activity4.py
@@ -0,0 +1,7 @@
+def circumference(radius):
+    if radius==0:
+        pass 
+    else:
+         print("circumference:" , 2*3.14*radius)
+radius = int(input("enter radius : "   ))
+circumference(radius)
